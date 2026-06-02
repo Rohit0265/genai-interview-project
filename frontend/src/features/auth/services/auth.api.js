4281cfce067701f7b31f321d087f56434dc6e85a
Rohit0265 <rohitmathur00981@gmail.com>
2026-06-02 22:49:50 +0530

    connecting with backend

---
+++ frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,49 @@
+import axios from "axios"
+
+export async function register({username,email,password}){
+    try {    
+        axios.post('http://localhost:3000/api/auth/register',{
+            username,email,password
+        },{
+            withCredentials:true
+        })
+    } 
+    catch (error) {
+        copnsole.log(error)
+    }
+}
+
+export async function login(email,password){
+    try {
+        const response = await axios.post('htpp://localhost:3000/api/auth/login',{
+            email , password
+        },{
+            withCredentials:true
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function logout(){
+    try {
+        const response = await axios.get("htpp://localhost:3000/api/auth/logout",{
+            withCredentials:true
+        })
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export async function getMe(){
+    try {
+        const response = await axios.get("htpp://localhost:3000/api/auth/get",{
+            withCredentials:true
+        })
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+}
