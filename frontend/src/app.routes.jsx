937e5fa21174c665b0aac7b0302f829afabbd945
Rohit0265 <rohitmathur00981@gmail.com>
2026-06-03 14:32:10 +0530

    added protector in home page

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
+import Protector from "./features/auth/components/Protector.jsx";
 
 export const router = createBrowserRouter([
     {
@@ -13,6 +14,9 @@ export const router = createBrowserRouter([
     },
     {
         path:"/",
-        element:<h1>Groveee street homeee</h1>
+        element:
+        <Protector>
+            <h1>Home Page</h1>
+        </Protector>
     }
 ])
---
+++ frontend/src/features/auth/components/Protector.jsx
@@ -0,0 +1,25 @@
+import { useAuth } from "../hooks/hooks.auth"
+import React from 'react'
+import { Navigate } from "react-router"
+const Protector = ({children}) => {
+    const {loading,user} = useAuth()
+    if(loading){
+        return (
+            <main>
+                <h1>
+                    Loading......
+                </h1>
+            </main>
+        )
+    }
+
+    if(!user){
+        return <Navigate to={"/login" }/>
+    }
+
+
+  return children
+
+}
+
+export default Protector
